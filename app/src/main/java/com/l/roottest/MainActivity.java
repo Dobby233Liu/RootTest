2ccf5fa57b8c3018b14a45fbe91aa7d1c9ce84a1
Dobby233Liu <papaqi1234567@126.com>
2018-02-04 15:59:47 +0800

    remove some useless code for less junk

--- app/src/main/java/com/l/roottest/MainActivity.java
+++ app/src/main/java/com/l/roottest/MainActivity.java
@@ -10,20 +10,8 @@ public class MainActivity extends Activity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-	TextView rootstr=(TextView)findViewById(R.id.RootString);
-	//TextView rootstria=(TextView)findViewById(R.id.RootStringInteractive);
-	Boolean hadsu=Shell.SU.available();
-	String suVersion = Shell.SU.version(false);
-	String suVersionInternal = Shell.SU.version(true);
-	String notroot=getString(R.string.notroot);
-	String rootenn=getString(R.string.rooted);
-	StringBuilder sb = (new StringBuilder()).append
-	(hadsu ? rootenn:notroot).append((char)10).
-	append(getString(R.string.suver)).append(suVersion == null ? "N/A" : suVersion).append((char)10).
-	append(getString(R.string.suintver)).append(suVersionInternal == null ? "N/A" : suVersionInternal).append((char)10);
-	rootstr.setText(sb.toString());
-	
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.main);
+    ((TextView)findViewById(R.id.RootString)).setText((new StringBuilder()).append(Shell.SU.available() ? getString(R.string.rooted):getString(R.string.notroot)).append((char)10).append(getString(R.string.suver)).append(Shell.SU.version(false) == null ? "N/A" : Shell.SU.version(false)).append((char)10).append(getString(R.string.suintver)).append(Shell.SU.version(true) == null ? "N/A" : Shell.SU.version(true)).append((char)10).toString());
     }
 }
