8bde39af8397fb3d21c3de706ce3777fe6f9ef35
Dobby233Liu <papaqi1234567@126.com>
2018-02-04 16:31:47 +0800

    try to fix that issue declared on release details

--- app/src/main/java/com/l/roottest/MainActivity.java
+++ app/src/main/java/com/l/roottest/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends Activity
     {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.main);
-    ((TextView)findViewById(R.id.RootString)).setText((new StringBuilder()).append(Shell.SU.available() ? getString(R.string.rooted):getString(R.string.notroot)).append((char)10).append(getString(R.string.suver)).append(Shell.SU.version(false) == null ? "N/A" : Shell.SU.version(false)).append((char)10).append(getString(R.string.suintver)).append(Shell.SU.version(true) == null ? "N/A" : Shell.SU.version(true)).append((char)10).toString());
+    ((TextView)findViewById(R.id.RootString)).setText((new StringBuilder()).append(Shell.SU.available() ? getString(R.string.rooted):getString(R.string.notroot)).append((char)10).append(getString(R.string.suver)+" ").append(Shell.SU.version(false) == null ? "N/A" : Shell.SU.version(false)).append((char)10).append(getString(R.string.suintver)+" ").append(Shell.SU.version(true) == null ? "N/A" : Shell.SU.version(true)).append((char)10).toString());
     }
 }
